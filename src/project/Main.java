aa204178abb1cc35629fff0b741d5f1dcbc24da3
Zahari <zemarinov@gmail.com>
2023-11-10 16:14:33 +0200

    add tests

--- src/project/Main.java
+++ src/project/Main.java
@@ -1,13 +1,35 @@
 package project;
 
+import jdk.jshell.execution.LoaderDelegate;
 import project.myparallelmergesort.ParallelMergesort;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.Temporal;
+import java.time.temporal.TemporalUnit;
 import java.util.Arrays;
 
 public class Main {
+
+    private static Integer[] generateRandomIntArray(int elements) {
+        Integer[] arr = new Integer[elements];
+        for(int i = 0; i < elements; i++) {
+            arr[i] = (int)(Math.random() * Integer.MAX_VALUE);
+        }
+        return arr;
+    }
+    private static void testMergeSort(int elements, int threads) {
+        Integer[] arr = generateRandomIntArray(elements);
+        LocalDateTime start = LocalDateTime.now();
+        ParallelMergesort.sort(arr, threads);
+        LocalDateTime end = LocalDateTime.now();
+        System.out.printf("%d elements sorted for %d milliseconds with %d threads%n", elements,
+                start.until(end, ChronoUnit.MILLIS), threads);
+    }
     public static void main(String args[]) {
-        Integer[] arr = {124,3412,32,5223,23};
-        ParallelMergesort.sort(arr, 1);
-        System.out.println(Arrays.toString(arr));
+        //System.out.println(Arrays.toString(generateRandomIntArray(5)));
+        testMergeSort(1000000, 1);
+        testMergeSort(1000000, 4);
     }
 }
